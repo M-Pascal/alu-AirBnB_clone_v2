3a64197cc30e78981572153d4efaf51a4b5e6f20
M-Pascal <p.mugisha@alustudent.com>
2024-02-10 12:16:07 -0800

    update

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage_t
 import json
 import os
 import pep8
