7f6fa313b0cf0fb4434151503b10897279e49281
M-Pascal <p.mugisha@alustudent.com>
2024-02-16 21:14:04 +0000

    2-task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """Fabric Script that distributes an archives"""
+from fabric.api import *
+import os
 
-from fabric.api import put, run, env
-from os.path import exists
 env.hosts = ['54.173.223.112', '3.80.135.235']
 
-
 def do_deploy(archive_path):
-    """Archives to web-servers"""
-    if exists(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        archive_name = os.path.basename(archive_path)
+        archive_name_no_ext = archive_name.split('.')[0]
+        release_path = '/data/web_static/releases/{}'.format(archive_name_no_ext)
+
+        run('mkdir -p {}'.format(release_path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_path))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv {}/web_static/* {}/'.format(release_path, release_path))
+        run('rm -rf {}/web_static'.format(release_path))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {} /data/web_static/current'.format(release_path))
+
         return True
     except:
         return False
